0d4e5115a68d8c0d084c98858a69a4218ccdfa3c
Florian Pommerening <florian.pommerening@gmail.com>
2015-03-28 21:04:12 +0100

    treated more code as general atuogenerated spl

--- quine/codesection.py
+++ quine/codesection.py
@@ -1,8 +1,6 @@
-import itertools
 from . import handwritten
 import spl.utilities
-from spl.generated import random_assignment_command, random_print_char_command, random_value_test_command, \
-    random_conditional_proceed_to_scene_command
+import spl.randomized
 
 
 def generate(prefix):
@@ -27,7 +25,7 @@ def generate(prefix):
     # Print prefix section
     code_lines = [
         handwritten.CODE_PRINT_PREFIX_START,
-        generate_print_statements(prefix, handwritten.CODE_PRINT_PREFIX_ACTORS),
+        spl.randomized.print_statements(prefix, handwritten.LITERALS, handwritten.CODE_PRINT_PREFIX_ACTORS),
         handwritten.CODE_PRINT_PREFIX_END,
     ]
 
@@ -44,23 +42,23 @@ def generate(prefix):
     ]
 
     # Utility section
-    number_scenes = [(spl.utilities.scene_number(i), literal)
+    number_scenes = [(literal, spl.utilities.scene_number(i))
                      for i, literal in enumerate(sorted(handwritten.LITERALS.values()), start=3)]
 
     code_lines += [
         handwritten.UTILITY_PRINT_PUSH_COMMAND_START,
-        generate_print_statements(handwritten.DATA_PUSH_COMMAND_BEFORE),
+        spl.randomized.print_statements(handwritten.DATA_PUSH_COMMAND_BEFORE, handwritten.LITERALS),
         handwritten.UTILITY_SWITCH_CASE_START,
-        generate_switch_statement(number_scenes),
+        spl.randomized.switch_statement(number_scenes),
         handwritten.UTILITY_SWITCH_CASE_END,
-        generate_print_statements(handwritten.DATA_PUSH_COMMAND_AFTER),
+        spl.randomized.print_statements(handwritten.DATA_PUSH_COMMAND_AFTER, handwritten.LITERALS),
         handwritten.UTILITY_PRINT_PUSH_COMMAND_END,
     ]
 
-    for scene_number, literal in number_scenes:
+    for literal, scene_number in number_scenes:
         code_lines += [
             handwritten.UTILITY_PRINT_LITERAL_START % scene_number,
-            generate_print_statements(literal),
+            spl.randomized.print_statements(literal, handwritten.LITERALS),
             handwritten.UTILITY_PRINT_LITERAL_END
         ]
 
@@ -69,47 +67,4 @@ def generate(prefix):
         handwritten.CODE_END_OF_PROGRAM
     ]
 
-    return "\n".join(code_lines)
-
-
-def generate_print_statements(text, actors=None):
-    """
-    Generate randomized SPL code to print the given text letter by letter.
-    :param text: the text to print
-    :param actors: the actors that should say the print line. If None, we
-                   assume the actor is already talking and add no line to
-                   indicate the actor. Otherwise, the list must contain either
-                   one actor whose line is added before all the print
-                   statements, or two actors who will take turns printing the
-                   text.
-    :return: a string containing code statements to print the text.
-    """
-    code_lines = []
-    if actors is None:
-        actors = [None]
-    elif len(actors) == 1:
-        code_lines = [actors[0] + ":"]
-    else:
-        assert len(actors) == 2
-
-    for actor, symbol in zip(itertools.cycle(actors), text):
-        if len(actors) > 1:
-            code_lines += [actor + ":"]
-        number = handwritten.LITERALS[ord(symbol)]
-        code_lines += ["\t" + random_assignment_command(number)]
-        code_lines += ["\t" + random_print_char_command()]
-    return "\n".join(code_lines)
-
-
-def generate_switch_statement(number_scenes):
-    """
-    Generate randomized SPL code for a switch statement that jumps to a different scene
-    for each literal.
-    :param number_scenes: list of tuples (s, l) where s is the label of the scene for literal l.
-    :return: a string containing code statements to jump to the correct scene.
-    """
-    code_lines = []
-    for scene_number, literal in number_scenes:
-        code_lines.append("\t" + random_value_test_command(literal))
-        code_lines.append("\t" + random_conditional_proceed_to_scene_command(scene_number))
     return "\n".join(code_lines)
---
+++ spl/randomized.py
@@ -0,0 +1,70 @@
+import itertools
+import random
+
+from . import tokens
+
+
+def assignment_command(value):
+    command = random.choice(tokens.ASSIGNMENT_COMMANDS)
+    adjective = random.choice(tokens.ADJECTIVES)
+    return command % (adjective, value)
+
+
+def print_char_command():
+    return random.choice(tokens.PRINT_CHAR_COMMANDS)
+
+
+def value_test_command(value):
+    command = random.choice(tokens.VALUE_TEST_COMMANDS)
+    adjective = random.choice(tokens.ADJECTIVES)
+    return command % (adjective, value)
+
+
+def conditional_proceed_to_scene_command(scene):
+    command = random.choice(tokens.CONDITIONAL_PROCEED_TO_SCENE_COMMANDS)
+    return command % scene
+
+
+def switch_statement(number_scenes):
+    """
+    Generate randomized SPL code for a switch statement that jumps to a different scene
+    for each value.
+    :param number_scenes: list of tuples (v, s) where v is a string representing
+                          a value and s is the label of the scene for v.
+    :return: a string containing code statements to jump to the correct scene.
+    """
+    code_lines = []
+    for value, scene_number in number_scenes:
+        code_lines.append("\t" + value_test_command(value))
+        code_lines.append("\t" + conditional_proceed_to_scene_command(scene_number))
+    return "\n".join(code_lines)
+
+
+def print_statements(text, literals, actors=None):
+    """
+    Generate randomized SPL code to print the given text letter by letter.
+    :param text: the text to print
+    :param literals: dictionary mapping ascii codes to shakespeare literals.
+    :param actors: the actors that should say the print line. If None, we
+                   assume the actor is already talking and add no line to
+                   indicate the actor. Otherwise, the list must contain either
+                   one actor whose line is added before all the print
+                   statements, or two actors who will take turns printing the
+                   text.
+    :return: a string containing code statements to print the text.
+    """
+    code_lines = []
+    if actors is None:
+        actors = [None]
+    elif len(actors) == 1:
+        code_lines = [actors[0] + ":"]
+    else:
+        assert len(actors) == 2
+
+    for actor, symbol in zip(itertools.cycle(actors), text):
+        if len(actors) > 1:
+            code_lines += [actor + ":"]
+        number = literals[ord(symbol)]
+        code_lines += ["\t" + assignment_command(number)]
+        code_lines += ["\t" + print_char_command()]
+    return "\n".join(code_lines)
